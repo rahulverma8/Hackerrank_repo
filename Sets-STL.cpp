825473d5198c8a3ff5a5ebd6b832bab360196b5c
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2023-12-02 22:50:35 +0530

    Update Sets-STL.cpp

--- Sets-STL.cpp
+++ Sets-STL.cpp
@@ -7,9 +7,8 @@
 using namespace std;
 
 int main() {
-  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
   int n, a, x, i;
-  set<int> s;
+  set<int> s; //set to store unique integers
   cin >> n;
   for (i = 0; i < n; i++) {
     cin >> a >> x;
